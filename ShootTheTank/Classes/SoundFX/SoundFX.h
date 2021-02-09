89d7c621dbef83c0b324412b3a77e244e104cc8e
WestleyK <westleyk@nym.hush.com>
2021-02-08 20:03:01 -0800

    Added blank soundFX class

---
+++ ShootTheTank/Classes/SoundFX/SoundFX.h
@@ -0,0 +1,16 @@
+//
+//  SoundFX.h
+//  ShootTheTank
+//
+//  Created by Westley Rose on 2021-02-08.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SoundFX : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ShootTheTank/Classes/SoundFX/SoundFX.m
@@ -0,0 +1,12 @@
+//
+//  SoundFX.m
+//  ShootTheTank
+//
+//  Created by Westley Rose on 2021-02-08.
+//
+
+#import "SoundFX.h"
+
+@implementation SoundFX
+
+@end
