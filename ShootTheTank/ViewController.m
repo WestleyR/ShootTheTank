5af4e3655db65fc58b1d5af38ccd4d54e3b58791
WestleyK <westleyk@nym.hush.com>
2021-02-10 18:05:46 -0800

    Added function to other upgrade buttons

--- ShootTheTank/ViewController.m
+++ ShootTheTank/ViewController.m
@@ -11,12 +11,17 @@
 @implementation ViewController
 
 // The amount of upgrade points avalible
-NSInteger upgradePointPool = 20;
+NSInteger upgradePointPool = 50;
 
 int tankClassIndex = 0;
 NSArray <NSString*>* tankClasses;
 
 int bulletDamageBarValue = 0;
+int bulletSpeedBarValue = 0;
+int tankArmorBarValue = 0;
+int maxHitpointsBarValue = 0;
+int tankSpeedBarValue = 0;
+int tankCamoBarValue = 0;
 
 - (void)viewDidLoad {
     NSLog(@"%s", __func__);
@@ -125,7 +130,6 @@ int bulletDamageBarValue = 0;
     upgradePointPool -= 5;
     self.UTPRemaningPointsLabel.stringValue = [NSString stringWithFormat:@"%d points left", (int)upgradePointPool];
 }
-
 - (IBAction)UTPBDMinuseAction:(id)sender {
     if (bulletDamageBarValue - 1 < 0) {
         return;
@@ -139,12 +143,144 @@ int bulletDamageBarValue = 0;
     self.UTPRemaningPointsLabel.stringValue = [NSString stringWithFormat:@"%d points left", (int)upgradePointPool];
 }
 
-
 // Bullet speed
+- (IBAction)UTPBSPlusAction:(id)sender {
+    if (upgradePointPool < 5) return;
+
+    if (bulletSpeedBarValue + 1 > self.UTPBulletSpeedBar.maxValue) {
+        return;
+    }
+
+    bulletSpeedBarValue++;
+
+    [self.UTPBulletSpeedBar setIntValue:bulletSpeedBarValue];
+
+    upgradePointPool -= 5;
+    self.UTPRemaningPointsLabel.stringValue = [NSString stringWithFormat:@"%d points left", (int)upgradePointPool];
+}
 - (IBAction)UTPBSMinusAction:(id)sender {
+    if (bulletSpeedBarValue - 1 < 0) {
+        return;
+    }
+
+    bulletSpeedBarValue--;
+
+    [self.UTPBulletSpeedBar setIntValue:bulletSpeedBarValue];
+
+    upgradePointPool += 5;
+    self.UTPRemaningPointsLabel.stringValue = [NSString stringWithFormat:@"%d points left", (int)upgradePointPool];
 }
 
-- (IBAction)UTPBSPlusAction:(id)sender {
+// Tank armor
+- (IBAction)UTPTAPlusAction:(id)sender {
+    if (upgradePointPool < 5) return;
+
+    if (tankArmorBarValue + 1 > self.UTPArmorBar.maxValue) {
+        return;
+    }
+
+    tankArmorBarValue++;
+
+    [self.UTPArmorBar setIntValue:tankArmorBarValue];
+
+    upgradePointPool -= 5;
+    self.UTPRemaningPointsLabel.stringValue = [NSString stringWithFormat:@"%d points left", (int)upgradePointPool];
+}
+- (IBAction)UTPTAMinusAction:(id)sender {
+    if (tankArmorBarValue - 1 < 0) {
+        return;
+    }
+
+    tankArmorBarValue--;
+
+    [self.UTPArmorBar setIntValue:tankArmorBarValue];
+
+    upgradePointPool += 5;
+    self.UTPRemaningPointsLabel.stringValue = [NSString stringWithFormat:@"%d points left", (int)upgradePointPool];
+}
+
+// Max hitpoints
+- (IBAction)UTPMHPlusAction:(id)sender {
+    if (upgradePointPool < 5) return;
+
+    if (maxHitpointsBarValue + 1 > self.UTPMaxHitpointsBar.maxValue) {
+        return;
+    }
+
+    maxHitpointsBarValue++;
+
+    [self.UTPMaxHitpointsBar setIntValue:maxHitpointsBarValue];
+
+    upgradePointPool -= 5;
+    self.UTPRemaningPointsLabel.stringValue = [NSString stringWithFormat:@"%d points left", (int)upgradePointPool];
+}
+- (IBAction)UTPMHMinusAction:(id)sender {
+    if (maxHitpointsBarValue - 1 < 0) {
+        return;
+    }
+
+    maxHitpointsBarValue--;
+
+    [self.UTPMaxHitpointsBar setIntValue:maxHitpointsBarValue];
+
+    upgradePointPool += 5;
+    self.UTPRemaningPointsLabel.stringValue = [NSString stringWithFormat:@"%d points left", (int)upgradePointPool];
+}
+
+// Tank speed
+- (IBAction)UTPTSPlusAction:(id)sender {
+    if (upgradePointPool < 5) return;
+
+    if (tankSpeedBarValue + 1 > self.UTPTankSpeedBar.maxValue) {
+        return;
+    }
+
+    tankSpeedBarValue++;
+
+    [self.UTPTankSpeedBar setIntValue:tankSpeedBarValue];
+
+    upgradePointPool -= 5;
+    self.UTPRemaningPointsLabel.stringValue = [NSString stringWithFormat:@"%d points left", (int)upgradePointPool];
+}
+- (IBAction)UTPTSMinusAction:(id)sender {
+    if (tankSpeedBarValue - 1 < 0) {
+        return;
+    }
+
+    tankSpeedBarValue--;
+
+    [self.UTPTankSpeedBar setIntValue:tankSpeedBarValue];
+
+    upgradePointPool += 5;
+    self.UTPRemaningPointsLabel.stringValue = [NSString stringWithFormat:@"%d points left", (int)upgradePointPool];
+}
+
+// Tank camo
+- (IBAction)UTPTCPlusAction:(id)sender {
+    if (upgradePointPool < 10) return;
+
+    if (tankCamoBarValue + 1 > self.UTPCamoBar.maxValue) {
+        return;
+    }
+
+    tankCamoBarValue++;
+
+    [self.UTPCamoBar setIntValue:tankCamoBarValue];
+
+    upgradePointPool -= 10;
+    self.UTPRemaningPointsLabel.stringValue = [NSString stringWithFormat:@"%d points left", (int)upgradePointPool];
+}
+- (IBAction)UTPTCMinusAction:(id)sender {
+    if (tankCamoBarValue - 1 < 0) {
+        return;
+    }
+
+    tankCamoBarValue--;
+
+    [self.UTPCamoBar setIntValue:tankCamoBarValue];
+
+    upgradePointPool += 10;
+    self.UTPRemaningPointsLabel.stringValue = [NSString stringWithFormat:@"%d points left", (int)upgradePointPool];
 }
 
 @end
